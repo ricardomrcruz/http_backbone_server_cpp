f256ef40c1f2b4ce77effd99d3498718af1574c6
ricardomrcruz <strasbourgwebsolutions@gmail.com>
2024-11-21 00:21:11 +0100

    connect/disconnect handlers

--- TcpListener.h
+++ TcpListener.h
@@ -1,5 +1,12 @@
 #pragma once
 
+#include <iostream>
+#include <WS2tcpip.h>
+#include <string>
+#include <sstream>
+
+#pragma comment (lib, "ws2_32.lib")
+
 
 class TcpListener 
 {
@@ -20,12 +27,20 @@ public:
 
 protected:
 
+	//handler for client connections
+	void onClientConnected();
+
+	//handler for client disconnections
+	void onClientDisconnected();
+
+	void sendToClient(int clientSocket, const char* msg, int length)
+
 private:
 
 	const char* m_ipAddress;    // IP Adress server will run on
 	int			m_port;			// Port # for the web service
 	int			m_socket;		// Internal FD for the listening socket
-
+	fd_set      m_master;       // Master file descriptor set
 
 
 
