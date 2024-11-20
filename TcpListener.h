ee8e8d7b2b0342417f710f998af47f76f1f62007
ricardomrcruz <strasbourgwebsolutions@gmail.com>
2024-11-20 04:35:38 +0100

    add tcplistener header and cpp file

--- TcpListener.h
+++ TcpListener.h
@@ -1,1 +1,33 @@
 #pragma once
+
+
+class TcpListener 
+{
+
+public:
+
+	TcpListener(const char* ipAddress, int port) :
+		m_ipAddress(ipAddress), m_port(port) { }
+
+	// initialize the listener
+	int init();
+
+	//run the listener
+	int run();
+
+
+
+
+protected:
+
+private:
+
+	const char* m_ipAddress;    // IP Adress server will run on
+	int			m_port;			// Port # for the web service
+	int			m_socket;		// Internal FD for the listening socket
+
+
+
+
+	
+};
